3452d4dcc279ef8e9beeaa2740b291b9f2d46ed0
Roman Thiele <r.thiele@bluewin.ch>
2017-02-20 21:52:04 +0100

    fixed some formation stuff

--- main.js
+++ main.js
@@ -19,8 +19,6 @@ module.exports.loop = function () {
     //    }
     //}
 
-    Game.spawns
-
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.memory.role == 'harvester') {
