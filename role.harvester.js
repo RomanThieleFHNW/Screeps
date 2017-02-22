9378e48e70f641cfc88bc4da091686358b928ebb
Roman Thiele <r.thiele@bluewin.ch>
2017-02-22 21:29:26 +0100

    bugfix

--- role.harvester.js
+++ role.harvester.js
@@ -4,7 +4,7 @@ var base = require('role.base');
 module.exports = {
 	/** @param {Creep} creep **/
 	run: function (creep) {
-		if (!creep.carry || (creep.carry.energy < creep.carryCapacity && !creep.memory.building)) {
+		if (!(creep.carry.energy) || (creep.carry.energy < creep.carryCapacity && !creep.memory.building)) {
 			base.harvestClosestSource(creep);
 			creep.memory.building = false;
 		} else {
